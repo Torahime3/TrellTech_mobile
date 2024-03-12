b9b209e882fc4daccee7667d61ac2fedb3670404
scottfzd <john.fitzgerald@epitech.eu>
2024-03-12 16:37:41 +0100

    add isHovered property and MouseRegion widget

--- trelltech/lib/pages/home.dart
+++ trelltech/lib/pages/home.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'package:trelltech/models/board_model.dart';
 import 'package:trelltech/controllers/board_controller.dart';
 
 import 'package:trelltech/pages/board.dart';
 import 'package:trelltech/widgets/appbar.dart';
-import 'package:trelltech/widgets/form.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,6 +16,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final BoardController _boardController = BoardController();
   List<BoardModel> boards = [];
+  bool _isHovered = false;
 
 
   @override
@@ -42,44 +43,52 @@ class _HomePageState extends State<HomePage> {
           itemBuilder: (BuildContext context, int index) {
             return Container(
               margin: const EdgeInsets.all(10),
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => BoardPage(boards[index])
-                    )
-                  );
-                },
-                child: Ink(
-                  height: 170,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.blue,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 6),
-                      ),
-                    ],
-                  ),
-                  child: Container(
-                    margin: const EdgeInsets.all(10),
-                    child: Text(
-                      boards[index].getName(),
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+              child: MouseRegion(
+                onEnter: (event) => setState(() {
+                  _isHovered = true;
+                }),
+                onExit: (event) => setState(() {
+                  _isHovered = false;
+                }),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => BoardPage(boards[index])
+                      )
+                    );
+                  },
+                  child: Ink(
+                    height: 170,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blue,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 6),
+                        ),
+                      ],
+                    ),
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      child: Text(
+                        boards[index].getName(),
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
+              )
             );
           },
-        ),
+      ),
     
       floatingActionButton: FloatingActionButton(
         onPressed: () {
