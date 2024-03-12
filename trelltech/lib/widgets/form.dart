25694e2d8c5d0572401c4f38b4b4fa098ac7860b
scottfzd <john.fitzgerald@epitech.eu>
2024-03-12 11:13:54 +0100

    rename widget 'form'

---
+++ trelltech/lib/widgets/form.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:trelltech/controllers/board_controller.dart';
+
+
+
+class CreateForm extends StatefulWidget {
+  const CreateForm({super.key});
+
+  @override
+  State<CreateForm> createState() => _CreateFormState();
+}
+
+class _CreateFormState extends State<CreateForm> {
+
+
+  final TextEditingController _textEditingController = TextEditingController();
+  final BoardController _boardController = BoardController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SizedBox(
+                height: 600,
+                child: Center(
+                  // child: Text('Your modal content goes here'),
+                  child: Form(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: "Board name",
+                            ),
+                            controller: _textEditingController,
+                            onFieldSubmitted: (String? value) {
+                              _boardController.create(value);
+                              Navigator.of(context).pop();
+                            },
+                          )
+                        )
+                      ],
+                    )
+                  )
+                )
+    );
+  }
+}
