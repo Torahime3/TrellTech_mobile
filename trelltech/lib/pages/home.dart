c62764a61efe2ab8029ab39b99b8337a01dff981
scottfzd <john.fitzgerald@epitech.eu>
2024-03-18 16:38:50 +0100

    add longPress to delete board from homePage

--- trelltech/lib/pages/home.dart
+++ trelltech/lib/pages/home.dart
@@ -17,15 +17,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final BoardController _boardController = BoardController();
   List<BoardModel> boards = [];
-  bool _isHovered = false;
 
   @override
   void initState() {
     super.initState();
-    _getInitialInfo();
+    _loadInfo();
   }
 
-  void _getInitialInfo() async {
+  void _loadInfo() async {
     final fetchedBoards = await _boardController.getBoards();
     setState(() {
       boards = fetchedBoards;
@@ -40,94 +39,106 @@ class _HomePageState extends State<HomePage> {
         body: ListView.builder(
           itemCount: boards.length,
           itemBuilder: (BuildContext context, int index) {
-            return Container(
-              margin: const EdgeInsets.all(10),
-              child: MouseRegion(
-                onEnter: (event) => setState(() {
-                  _isHovered = true;
-                }),
-                onExit: (event) => setState(() {
-                  _isHovered = false;
-                }),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => BoardPage(board: boards[index])
-                      )
-                    );
+              return Container(
+                  margin: const EdgeInsets.all(10),
+                  child: GestureDetector(
+                    onLongPress: () {
+                        showMenu(
+                          context: context, 
+                          position: const RelativeRect.fromLTRB(0, 200, 0, 0), 
+                          items: <PopupMenuEntry>
+                                  [
+                                    PopupMenuItem(child: ListTile(
+                                      title: const Text('Delete board'),
+                                      onTap: () {
+                                        _boardController.delete(boards[index].id);
+                                        _loadInfo();
+                                        Navigator.of(context).pop();
+                                      }
+                                    )),
+                                  ]
+                        );
+                      },
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => BoardPage(board: boards[index])
+                          )
+                        );
 
-                  },
-                  child: Ink(
-                    height: 170,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 6),
+                      },
+                      child: Ink(
+                        height: 170,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blue,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: const Offset(0, 6),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    child: Container(
-                      margin: const EdgeInsets.all(10),
-                      child: Text(
-                        boards[index].getName(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                        child: Container(
+                          margin: const EdgeInsets.all(10),
+                          child: Text(
+                            boards[index].getName(),
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
 
-                ),
-              )
-            );
-          },
-      ),
-    
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (BuildContext context) {
-              return SizedBox(
-                height: 600,
-                child: Center(
-                  // child: Text('Your modal content goes here'),
-                  child: Form(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "Board name",
-                            ),
-                            onFieldSubmitted: (String value) {
-                              _boardController.create(value);
-                              Navigator.of(context).pop();
-                            },
-                          )
-                        )
-                      ],
-                    )
+                    ),
                   )
-                )
+                );
+              },
+          ),
+        
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) {
+                  return SizedBox(
+                    height: 600,
+                    child: Center(
+                      // child: Text('Your modal content goes here'),
+                      child: Form(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: "Board name",
+                                ),
+                                onFieldSubmitted: (String value) {
+                                  _boardController.create(value);
+                                  Navigator.of(context).pop();
+                                  _loadInfo();
+                                },
+                              )
+                            )
+                          ],
+                        )
+                      )
+                    )
+                  );
+                }
               );
-            }
-          );
-        },
-        tooltip: 'Increment Counter',
-        backgroundColor: const Color.fromARGB(255, 229, 229, 229),
-        child: const Icon(Icons.add),
-      )
-    );
+            },
+            tooltip: 'Increment Counter',
+            backgroundColor: const Color.fromARGB(255, 229, 229, 229),
+            child: const Icon(Icons.add),
+          )
+        );
   }
 }
