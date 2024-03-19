04e824eda53f304ca26132595efbbb618481c086
scottfzd <john.fitzgerald@epitech.eu>
2024-03-19 14:18:43 +0100

    add menu button in appbar on boardPage

--- trelltech/lib/widgets/appbar.dart
+++ trelltech/lib/widgets/appbar.dart
@@ -1,36 +1,50 @@
-// import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 // import 'package:trelltech/widgets/form.dart';
+enum SampleItem { update, delete }
+SampleItem? selectedItem;
 
 AppBar appbar({dynamic text = "TrellTech", color = Colors.transparent, bool showEditButton = false, onEdit, onDelete}) {
-  List<Widget> actions = [];
 
-  if (showEditButton) {
-    actions.add(
-      IconButton(
-        icon: const Icon(Icons.edit),
-        onPressed: onEdit,
-      ),
-    );
-    actions.add(
-      IconButton(
-        icon: const Icon(Icons.delete),
-        onPressed: onDelete,
-      )
-    );
-  }
-    
+  List<Widget> actions = [];
+    if (showEditButton) {
+      actions.add(
+        PopupMenuButton<SampleItem>(
+          onSelected: (SampleItem item) {
+            switch(item) {
+              case SampleItem.update:
+                onEdit();
+                break;
+              case SampleItem.delete:
+                onDelete();
+                break;
+            }
+          },
+          itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
+            const PopupMenuItem<SampleItem>(
+              value: SampleItem.update,
+              child: Text('Edit'),
+            ),
+            const PopupMenuItem<SampleItem>(
+              value: SampleItem.delete,
+              child: Text('Delete'),
+            ),
+          ]
+        )
+      );
+    }
   return AppBar(
     title: Text(text,
         style: const TextStyle(
           color: Color.fromARGB(255, 34, 34, 34),
           fontSize: 24,
           fontWeight: FontWeight.bold,
-        )),
+        )
+    ),
     centerTitle: true,
     backgroundColor: color,
     elevation: 0,
     actions: actions
   );
 }
+
+
