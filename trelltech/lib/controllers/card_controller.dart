11fc0d4b0336826433bfe63b7a772664750e21fa
Nialoshaar <valerian.alamar@epitech.eu>
2024-03-28 04:06:13 +0100

    Update Card controller

--- trelltech/lib/controllers/card_controller.dart
+++ trelltech/lib/controllers/card_controller.dart
@@ -2,7 +2,6 @@
 
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:trelltech/models/card_model.dart';
